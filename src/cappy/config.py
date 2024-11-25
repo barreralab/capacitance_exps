d7dfda2b921744ff2243065491cafd4fd8b008d5
fishyvishy <vishy235@gmail.com>
2024-11-25 16:10:49 -0500

    ready for ppms

--- src/cappy/config.py
+++ src/cappy/config.py
@@ -12,4 +12,8 @@ datapath = root / "data"
 qc_dbpath = datapath / "qc.db"
 mm_dpath = datapath / "mm"
 
-testconfig = root / "src" / "cappy" / "station_configs" / "insts.yml"
+testconfig = (
+    root / "src" / "cappy" / "station_configs" / "insts.yml"
+)  # insts.yml is the config file which has all our addresses
+
+coolconfig = root / "src" / "cappy" / "station_configs" / "ppms_exps.yml"
