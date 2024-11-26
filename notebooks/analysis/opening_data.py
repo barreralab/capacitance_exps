b5311bc81de5837c94625ab8b5b22b902fc32823
fishyvishy <vishy235@gmail.com>
2024-11-25 22:30:20 -0500

    ppms time

--- notebooks/analysis/opening_data.py
+++ notebooks/analysis/opening_data.py
@@ -1,10 +1,10 @@
 from cappy.config import datapath
+from sweep.sweep_load import pload1d
+from time import strftime, localtime
 import numpy as np
 
 # Path to your TSV.gz file
-file_path = datapath / "24" / "data.tsv.gz"
-data = np.transpose(np.loadtxt(file_path))
-Vout_lockin = data[4]
-print(np.argmax(Vout_lockin))
-Vg = data[1]
-print(Vg[93])
+idx = 24
+file_path = datapath / "test_archive"
+data = pload1d(file_path, idx)
+print(strftime("%Y-%m-%d %H:%M:%S", localtime(data["time"][0])))
