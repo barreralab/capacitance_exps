bb6c7c613275f8397eff8e08847cd02187145cd5
eames <eames.donovan@mail.utoronto.ca>
2025-06-11 16:54:55 -0400

    added off balance cap and cond

---
+++ cappy/.ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,15 @@
+"""
+    configuration file 
+"""
+
+from pathlib import Path
+
+root = Path(
+    "C:/Users/barreralab/OneDrive - University of Toronto/Documents/capacitance_exps"
+)
+
+datapath = root / "data"
+qc_dbpath = datapath / "qc.db"
+mm_dpath = datapath / "mm"
+
+testconfig = root / "src" / "cappy" / "station_configs" / "insts.yml"
